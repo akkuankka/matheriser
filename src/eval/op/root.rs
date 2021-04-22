b6db658c3ab6df69dab2227ec8cbbc6a134a9ce8
akkuankka <cwmckenzie02@gmail.com>
2021-04-22 17:10:49 +0930

    Added a function to generate parts of pascal's triangle
    This will be used in an algorithm to determine if a number is n-cubic
    (i.e. results from raising a natural to a natural power)

--- src/eval/op/root.rs
+++ src/eval/op/root.rs
@@ -23,6 +23,36 @@ impl NthRoot for f64 {
         }
     }
 }
+/* big maths time: whether a number is square, cube,
+ etc can be determined by whether it can be satisfied
+ by a sum of subsequent results of this formula: where P(a:b)
+ is the bth element of pascal's triangle row a
+
+ (k + 1)^n - k(n) = P(n:1)*k^(n-1) + P(n:2)*k^(n-2) + ...+ P(n:n-1)*k + 1
+ e.g. (k+1)^2 - k^2 = 2k+1 or
+      (k+1)^3 - k^3 = 3k^2 + 3k + 1 
+ thus if you go through values of k until you either
+ exceed or equal your target number you know if it is n-cubic */
+
+/// Generates the non-1 elements of pascals triangle for a row `n` 
+fn generate_pascals_row_inners(n: u32) -> Vec<u32> {
+    if (n == 0) | (n == 1) {return vec![0]}
+    else {
+        let use_symmetry_point = (n as f64 / 2.).ceil() as u32;
+        let mut result = vec![n];
+        let mut reflection_counter = 1_u32;
+        for i in 2..=n {
+           let prev = *result.last().unwrap(); // we put something in the vec just then
+           let current = prev * ((n + 1 - i)/i);
+           result.push(current);
+        }
+        result
+    }
+}
+
+fn is_n_dim_cube(n: u32, dimension: u32) -> bool {
+    
+}
 
 impl NthRoot for i64 {
     type Output = Option<Self> {
