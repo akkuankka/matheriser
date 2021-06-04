272132073532887e21c950f80069a1298a929dc2
akkuankka <cwmckenzie02@gmail.com>
2021-06-04 22:00:15 +0930

    Added localisation manifest using RON
    Currently this is unused, as there is only one language supported and there's
    only one piece of text, but in the future error refactoring etc. there will be
    more internationalised text, and more text that isn't hard coded.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
 #![feature(negative_impls)]
 
+use ron::de::from_str;
+use serde::Deserialize;
+use std::collections::HashMap;
 mod parser;
 mod eval;
 mod util;
 mod frontend;
 fn main() {
+    // first, get the localisation file from disk
+    let manifest_path = Path::new("assets/en_uk.ron");
+    let manifest_file = match File::open(manifest_path) {
+        Err(reason) => {
+            println!("{}", format!("Aborting: could not open file -- {}", reason).red());
+            std::process::exit(1);
+        }
+        Ok(file) => file
+    };
+    let mut manifest_str = String::new();
+    if let Err(reason) = manifest_file.read_to_string(&mut manifest_str) {
+        println!("{}", format!("Aborting: could read file -- {}", reason).red());
+        std::process::exit(1);
+    }
+    // parse it as a hashmap
+    let manifest: HashMap<&str, String> = match from_str(&manifest_str) {
+        Err(e) => {
+            println!("{}", format!("Developer error: localisation file is bad -- {}", e).red());
+            std::process::exit(1);
+        }
+        Ok(map) => map
+    };
 
 }
