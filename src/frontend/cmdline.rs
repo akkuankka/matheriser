6d1f9fb917b4009caa525987f90ad7701f02add2
akkuankka <cwmckenzie02@gmail.com>
2021-06-09 20:57:12 +0930

    Changed commandline frontend to be better
    Uses crate Dialoguer

--- src/frontend/cmdline.rs
+++ src/frontend/cmdline.rs
@@ -8,7 +8,7 @@ pub struct CommandLine<'m, 'k: 'm> {
     manifest: &'m HashMap<&'k str, String>,
 }
 impl<'m, 'k> CommandLine<'m, 'k> {
-    const PROMPT_TEXT: &'static [u8] = b"matherise >";
+    const PROMPT_TEXT: &'static str = "matherise";
 
     pub fn new(manifest: &'m HashMap<&'k str, String>) -> Self {
         CommandLine {
@@ -16,66 +16,38 @@ impl<'m, 'k> CommandLine<'m, 'k> {
             manifest: manifest,
         }
     }
-
-    fn prompt(&self, f: &Stdout) {
-        println!("prompting !");
-        let mut handle = f.lock();
-        let _= handle.write_all(Self::PROMPT_TEXT);
-        let _= handle.flush();
-    }
-
-    fn input(&self, f: &Stdin) -> Result<String, &'static str> {
-        let buffer = &mut String::with_capacity(10);
-        match f.read_line(buffer) {
-            Err(_) => return Err("Error: couldn't read from stdin"),
-            Ok(_) => {}
-        }
-        Ok(buffer.trim().to_string())
-    }
 }
 
 use crate::parser::parse_string;
 use colored::Colorize;
-use std::io::{self, Stdin, Stdout, Write};
-#[allow(unused_must_use)]
+use dialoguer::{theme::ColorfulTheme, Input};
 impl<'m, 'k> super::Frontend for CommandLine<'_, '_> {
-    fn run(&mut self) -> Result<(), &'static str> {
+    fn run(&mut self) -> Result<(), String> {
         //println!("began to run");
-        let stdout = io::stdout();
         if self.preamble {
-            stdout.lock().write_all(
-                self.manifest
-                    .get("preamble")
-                    .ok_or("Developer error: preamble not found in language file")?
-                    .blue()
-                    .as_bytes(),
-            );
+            let coloured_preamble = self
+                .manifest
+                .get("preamble") // get the preamble
+                .ok_or("couldn't get the preamble")? // unwrap it
+                .green();
+            println!("{}", coloured_preamble)
             //println!("preambled");
         }
-        let stdin = io::stdin();
-        let mut locked_stdout = stdout.lock();
         loop {
-        //    println!("looping");
-            self.prompt(&stdout);
-            let input = self.input(&stdin)?;
+            let input_result: Result<String, _> = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt(CommandLine::PROMPT_TEXT)
+                .interact_text();
+            let input = match input_result {
+                Ok(s) => s,
+                Err(_) => {
+                    return Err("Couldn't get your input".into());
+                }
+            };
             if input == "!qt" {
                 break;
-            } else {
-                match parse_string(&input) {
-                    Err(e) => {
-                        locked_stdout.write_all(e.red().as_bytes());
-                    }
-                    Ok(t) => match t.eval() {
-                        Err(e) => {
-                            locked_stdout.write_all(e.red().as_bytes());
-                        }
-                        Ok(value) => {
-                            locked_stdout.write_all(format!("{}", value).blue().as_bytes());
-                        }
-                    },
-                }
-                locked_stdout.flush();
             }
+            let out_text = parse_string(&input).and_then(|x| x.eval())?;
+            println!("         >=> {}", out_text);
         }
         Ok(())
     }
--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -5,5 +5,5 @@ pub use display::*;
 pub use cmdline::CommandLine;
 
 pub trait Frontend {
-    fn run(&mut self) -> Result<(), &'static str>;
+    fn run(&mut self) -> Result<(), String>;
 }
