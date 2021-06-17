e641769d41fa0afa37b06041e1ad44c5fb5fcea1
akkuankka <cwmckenzie02@gmail.com>
2021-06-17 11:53:54 +0930

    Added comments and cargo install

--- install-linux.sh
+++ install-linux.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# Cargo install the package
+cargo install --path .
+# create the directories for the language files
 mkdir ~/.local/share/matheriser/ && mkdir ~/.local/share/matheriser/assets/
+# copy the language files across
 cp assets/*.ron ~/.local/share/matheriser/assets/
+# remove the manifest, it's hard-coded
 rm ~/.local/share/matheriser/assets/manifest.ron
